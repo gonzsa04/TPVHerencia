52bb8dd5021f72ed702497f6d2711695dccdd3ef
Gonzalo <gonzsa04@ucm.es>
2017-12-17 17:52:44 +0100

    Fantasmas envejecen

--- HolaSDL/SmartGhost.cpp
+++ HolaSDL/SmartGhost.cpp
@@ -17,23 +17,54 @@ void SmartGhost::saveToFile(ofstream& file)
 	file << " " << edad;
 }
 
+void SmartGhost::envejece()
+{
+	if(edad == edadAd)
+	{
+		col = 1;
+		adulto = true;
+	}
+	else if (edad >= edadMax)
+	{
+		muerto = true;
+		fil = 5;
+	}
+	edad++;
+}
+
+void SmartGhost::morir()
+{
+	edad = 0;
+	col = 0;
+	fil = 4;
+	adulto = false; 
+	muerto = false;
+	Fantasma::morir();
+}
+
 void SmartGhost::update()
 {
-	posiblesDirecciones();//establecemos las posibles direcciones que puede tomar el fantasma
+	if (adulto) {
+		posiblesDirecciones();//establecemos las posibles direcciones que puede tomar el fantasma
 
-	bool encontrada = false;
-	int i = 0;
-	while (i < posiblesDir.size() && !encontrada) {
-		dirX = posiblesDir[i].x;
-		dirY = posiblesDir[i].y;
-		if (abs((posiblesDir[i].x + destRect.x) - game->getPacman()->getPosX()) < abs(destRect.x - game->getPacman()->getPosX()) ||
-			abs((posiblesDir[i].y + destRect.y) - game->getPacman()->getPosY()) < abs(destRect.y - game->getPacman()->getPosY())) {
-			encontrada = true;
+		bool encontrada = false;
+		int i = 0;
+		while (i < posiblesDir.size() && !encontrada && !muerto)
+		{
+			dirX = posiblesDir[i].x;
+			dirY = posiblesDir[i].y;
+			if (abs((dirX + destRect.x) - game->getPacman()->getPosX()) < abs(destRect.x - game->getPacman()->getPosX()) ||
+				abs((dirY + destRect.y) - game->getPacman()->getPosY()) < abs(destRect.y - game->getPacman()->getPosY()))
+				encontrada = true;
+			i++;
 		}
-		else i++;
+		if (!muerto)
+		{
+			destRect.x += dirX;
+			destRect.y += dirY;
+		}
+		Personaje::update();//vemos si se sale del mapa
 	}
-	destRect.x += dirX;
-	destRect.y += dirY;
-
-	Personaje::update();//miramos si se ha salido del mapa
+	else Fantasma::update();
+	envejece();
 }
--- HolaSDL/SmartGhost.h
+++ HolaSDL/SmartGhost.h
@@ -5,6 +5,10 @@ class SmartGhost : public Fantasma
 {
 private:
 	int edad;
+	int edadAd = 60;
+	int edadMax = 250;
+	bool adulto = false;
+	bool muerto = false;
 public:
 	SmartGhost() :Fantasma() {};
 	SmartGhost(Game* game, Texture* text1, Texture* text2, int width, int height, int f, int c, int numT, int numF) :
@@ -12,4 +16,6 @@ public:
 	virtual void loadFromFile(ifstream& file);
 	virtual void saveToFile(ofstream& file);
 	virtual void update();
+	void envejece();
+	void morir();
 };
